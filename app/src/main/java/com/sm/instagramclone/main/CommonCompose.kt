b334b9c7e58236af206ac50150a7f861a9122391
krishnaji <prahaladsharma4u@gmail.com>
2024-02-12 20:09:37 +0530

    SignUp UI Design.

--- app/src/main/java/com/sm/instagramclone/main/CommonCompose.kt
+++ app/src/main/java/com/sm/instagramclone/main/CommonCompose.kt
@@ -1,7 +1,17 @@
 package com.sm.instagramclone.main
 
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.sm.instagramclone.IgViewModel
 
@@ -11,5 +21,20 @@ fun NotificationMessage(viewModel: IgViewModel){
     val notifyMessage = notifyState?.getContentOrNull()
     if (notifyMessage != null){
         Toast.makeText(LocalContext.current, notifyMessage, Toast.LENGTH_LONG).show()
+    }
+}
+
+@Composable
+fun CommonProgressSpinner(){
+    Row(
+        modifier = Modifier
+            .alpha(0.5f)
+            .background(Color.LightGray)
+            .clickable { }
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CircularProgressIndicator()
     }
 }
